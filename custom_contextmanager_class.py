a52dc597abc287729d6a363bdbe02aa5c176828e
Mohsin Ali <dev.mohsinali@gmail.com>
2022-06-28 08:51:37 +0500

    context manager usage and creating custom context manager class

---
+++ custom_contextmanager_class.py
@@ -0,0 +1,18 @@
+class ManagedFile:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __enter__(self):  # called as soon as it enters with block
+        print("__enter__ called")
+        self.file = open(self.filename, 'w')
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):  # called when exited with block
+        if self.file:
+            self.file.close()
+        print("__exit__ called")
+        # return True  #if True is returned from exit exception raised here is ignored
+
+
+with ManagedFile("myfile.txt") as file:
+    file.write("some text")
---
+++ custom_contextmanager_with_generator.py
@@ -0,0 +1,14 @@
+from contextlib import contextmanager
+
+
+@contextmanager
+def managed_file(filename):
+    try:
+        file = open(filename, "w")
+        yield file
+    finally:
+        file.close()
+
+
+with managed_file("myfile.txt") as my_file:
+    my_file.write("some text")
